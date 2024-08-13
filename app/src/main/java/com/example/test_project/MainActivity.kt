12bca17d3a05e90a5117d4c77abf27357e618ac6
arukas1029 <arukas1029@hanmail.net>
2024-08-13 16:58:01 +0900

    Add login page with form validation

--- app/src/main/java/com/example/test_project/MainActivity.kt
+++ app/src/main/java/com/example/test_project/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello World $name!",
+        text = "Hello World World $name!",
         modifier = modifier
     )
 }
